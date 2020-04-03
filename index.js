99927fd9ace7b29145ed1140c5f4221add462bbd
Maurizio Tonelli <mauriziotonelli@hotmail.com>
2020-04-03 15:01:29 -0600

    Practica 7

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const express = require('express');
 const app = express();
-const {pokemon} = require('./pokedex.json');
+const pokemon = require('./routes/pokemon');
 
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -10,32 +12,7 @@ app.get("/",(req,res,next)=>{
     return res.status(200).send("Bienvenido a Pokedex");
 });
 
-app.post('/pokemon',(req, res, next)=>{
-    return res.status(200).send(req.body);
-});
-
-app.get('/pokemon',(req, res, next)=>{
-    return res.status(200).send(pokemon);
-});
-
-app.get('/pokemon/:id([0-9]{1,3})', (req, res, next)=>{
-    const id = req.params.id -1;
-    if(id > 0 && id <=150){
-        return res.status(200).send(pokemon[id]);
-    }
-    return res.status(404).send("Pokemon no encontrado");
-});
-
-app.get('/pokemon/:name([A-Za-z]+)', (req,res, next)=>{
-    const name = req.params.name;
-
-    const pk = pokemon.filter((p)=>{
-        return p.name.toUpperCase() == name.toUpperCase();
-    });
-
-    return (pk.length > 0) ? 
-        res.status(200).send(pk) : res.status(404).send("pokemon no encontrado");
-});
+app.use("/pokemon", pokemon);
 
 app.listen(process.env.PORT || 3000, ()=>{
     console.log("server is running");
