089d337a29ab853c941f351c45ab11770b995f9a
Maurizio Tonelli <mauriziotonelli@hotmail.com>
2020-05-07 16:04:53 -0500

    practica 13

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const user = express.Router();
 const db = require('../config/database');
 
@@ -14,6 +15,24 @@ user.post("/",async (res,req,next)=>{
         return res.status(500).json({code:500, message:" error"});
     }
     return res.status(500).json({code:500, message:" campos incompletos"});
+});
+user.post("/login",async(res,req,next)=>{
+    const {user_mail, user_password}=req.body;
+    const query = `SELECT * FROM user WHERE user_mail='${user_mail}' AND user_password='${user_password}';`;
+    const rows = await db.query(query);
+
+    if(user_mail && user_password){
+        if(rows.length== 1){
+            const token = jwt.sign({
+                user_id : rows[0].user_id,
+                user_mail : rows[0].user_mail
+            }, "debugkey");
+            return res.status(200).json({code:200, message: token})
+        }else{
+            return res.status(401).json({code:401, message: "usuario o contraseña incorrectos"})
+        }
+    }
+    return res.status(500).json({code:500, message: "campos incompletos"})
 });
 user.get("/", async (res,req,next)=>{
     const query = "SELECT * FROM user";
